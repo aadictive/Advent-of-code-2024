191c7ab96efa435a97adf95f37efc7f332a67686
Aditya Chaudhari <asc4896@gmail.com>
2024-12-02 10:02:03 -0500

    Day 1 Part 1 & 2 solutions

--- solutions/solution_day1_part1.py
+++ solutions/solution_day1_part1.py
@@ -3,10 +3,22 @@ from utility.parseFile import *
 
 # Problem Statement: https://adventofcode.com/2024/day/1
 def solution(identifier):
-    input = parse_file("day1_part1.txt", identifier)
+    input = parse_file("day1.txt", identifier)
     # Write Solution here
+    left_list, right_list = [], []
+    for inp in input:
+        strip_new_lines = inp.strip()
+        llist, rlist = strip_new_lines.split("   ")
+        left_list.append(int(llist))
+        right_list.append(int(rlist))
+    left_list.sort()
+    right_list.sort()
 
-    return 0
+    sum = 0
+    for l, r in zip(left_list, right_list):
+        sum += abs(l-r)
+
+    return sum
 
 
 def executeOnExampleFile():
--- solutions/solution_day1_part2.py
+++ solutions/solution_day1_part2.py
@@ -4,10 +4,20 @@ from utility.parseFile import *
 
 # Problem Statement: https://adventofcode.com/2024/day/1
 def solution(identifier):
-    input = parse_file("day1_part2.txt", identifier)
+    input = parse_file("day1.txt", identifier)
     # Write Solution here
+    left_list, right_list = [], []
+    for inp in input:
+        strip_new_lines = inp.strip()
+        llist, rlist = strip_new_lines.split("   ")
+        left_list.append(int(llist))
+        right_list.append(int(rlist))
 
-    return 0
+    similarity_score = 0
+    for l, r in zip(left_list, right_list):
+        similarity_score += l * right_list.count(l)
+
+    return similarity_score
 
 
 def executeOnExampleFile():
