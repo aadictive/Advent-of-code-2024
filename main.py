84381d9e0718631283acf0f94c5d4103f368f654
Aditya Chaudhari <asc4896@gmail.com>
2024-12-03 09:46:13 -0500

    Refactor main.py file for running solutions using command line args

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from solutions import *
-import os
+import os, sys
 base_path = "solutions/"
 
 def print_solution(day, part):
@@ -24,28 +24,33 @@ def print_solution(day, part):
         print("Invalid Selection!")
 
 if __name__ == "__main__":
-    print("###############################################################")
-    print(" Welcome to Advent of Code 2024 solutions by Aditya Chaudhari ")
-    print("###############################################################")
-    print("Select appropriate option to run the solution:")
-    print("\t1 - Run all available solutions and print answers.\n\t2 - Run a particular solution.\n\t0 - Exit!")
-    usr_selection = input("Enter your value: ")
-    if usr_selection == "1":
-        selection = "all"
-    elif usr_selection == "2":
-        selection = "particular"
-        day = input("Run solution for which day? (Select between 1-25): ")
-        print("Run solution for which part?\n\t1 - Part 1\n\t2 - Part 2\n\t3 - Both parts!\n\t0- Exit!")
-        part = input("Enter your value: ")
-        if part == "0":
-            print("Exiting gracefully!")
-            exit(0)
-    elif usr_selection == "0":
-            print("Exiting gracefully!")
-            exit(0)
+    if len(sys.argv) > 1:
+        selection = sys.argv[1]
+        day = sys.argv[2]
+        part = sys.argv[3]
     else:
-        print("Invalid Selection!")
-        exit()
+        print("###############################################################")
+        print(" Welcome to Advent of Code 2024 solutions by Aditya Chaudhari ")
+        print("###############################################################")
+        print("Select appropriate option to run the solution:")
+        print("\t1 - Run all available solutions and print answers.\n\t2 - Run a particular solution.\n\t0 - Exit!")
+        usr_selection = input("Enter your value: ")
+        if usr_selection == "1":
+            selection = "all"
+        elif usr_selection == "2":
+            selection = "particular"
+            day = input("Run solution for which day? (Select between 1-25): ")
+            print("Run solution for which part?\n\t1 - Part 1\n\t2 - Part 2\n\t3 - Both parts!\n\t0- Exit!")
+            part = input("Enter your value: ")
+            if part == "0":
+                print("Exiting gracefully!")
+                exit(0)
+        elif usr_selection == "0":
+                print("Exiting gracefully!")
+                exit(0)
+        else:
+            print("Invalid Selection!")
+            exit()
 
     match selection:
         case "all":
