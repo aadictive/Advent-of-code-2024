9a7259d44c9bcf528a3199bb9d2be968e2dd9ee1
Aditya Chaudhari <asc4896@gmail.com>
2024-12-03 10:33:16 -0500

    Add unit tests, fix code syntax for warnings

---
+++ test_solutions.py
@@ -0,0 +1,58 @@
+from solutions import *
+import unittest
+import tracemalloc
+
+
+class MyTestCase(unittest.TestCase):
+
+    def setUp(self):
+        tracemalloc.start()
+    def test_example_files(self):
+        result = solution_day1_part1.execute_example_file()
+        self.assertEqual(11, result)
+
+        result = solution_day1_part2.execute_example_file()
+        self.assertEqual(31, result)
+
+        result = solution_day2_part1.execute_example_file()
+        self.assertEqual(2, result)
+
+        result = solution_day2_part2.execute_example_file()
+        self.assertEqual(4, result)
+
+        result = solution_day3_part1.execute_example_file()
+        self.assertEqual(161, result)
+
+        result = solution_day3_part2.execute_example_file()
+        self.assertEqual(161, result)
+
+    def test_puzzle_files(self):
+        result = solution_day1_part1.execute_puzzle_file()
+        self.assertEqual(2378066, result)
+
+        result = solution_day1_part2.execute_puzzle_file()
+        self.assertEqual(18934359, result)
+
+        result = solution_day2_part1.execute_puzzle_file()
+        self.assertEqual(490, result)
+
+        result = solution_day2_part2.execute_puzzle_file()
+        self.assertEqual(536, result)
+
+        result = solution_day3_part1.execute_puzzle_file()
+        self.assertEqual(183380722, result)
+
+        result = solution_day3_part2.execute_puzzle_file()
+        self.assertEqual(82733683, result)
+
+    def tearDown(self):
+        snapshot = tracemalloc.take_snapshot()
+        top_stats = snapshot.statistics('lineno')
+
+        print("[ Top 10 ]")
+        for stat in top_stats[:10]:
+            print(stat)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- utility/parseFile.py
+++ utility/parseFile.py
@@ -12,6 +12,6 @@ def parse_file(filename, identifier):
         full_path = path.joinpath(exampleFilesPath, filename)
     elif identifier == PUZZLE_FILE_IDENTIFIER:
         full_path = path.joinpath(puzzleFilesPath, filename)
-    lines = open(full_path, "r")
-    puzzle_input = lines.readlines()
+    with open(full_path, "r") as lines:
+        puzzle_input = lines.readlines()
     return puzzle_input
