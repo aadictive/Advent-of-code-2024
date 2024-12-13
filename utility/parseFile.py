b6e8017119b217552ad55c6c7422a28fa90b528b
Aditya Chaudhari <asc4896@gmail.com>
2024-12-13 16:29:14 +0530

    Add read_file utility method

--- utility/parseFile.py
+++ utility/parseFile.py
@@ -15,3 +15,14 @@ def parse_file(filename, identifier):
     with open(full_path, "r") as lines:
         puzzle_input = lines.readlines()
     return puzzle_input
+
+
+def read_file(filename, identifier):
+    full_path = ""
+    if identifier == EXAMPLE_FILE_IDENTIFIER:
+        full_path = path.joinpath(exampleFilesPath, filename)
+    elif identifier == PUZZLE_FILE_IDENTIFIER:
+        full_path = path.joinpath(puzzleFilesPath, filename)
+    with open(full_path, "r") as lines:
+        puzzle_input = lines.read()
+    return puzzle_input.splitlines()
