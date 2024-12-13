49169384c76a4fd260f0afa2b05c29bfaa14a5d0
Aditya Chaudhari <asc4896@gmail.com>
2024-12-13 17:09:30 +0530

    Update read_file util for splitlines as a optional param

--- utility/parseFile.py
+++ utility/parseFile.py
@@ -17,7 +17,7 @@ def parse_file(filename, identifier):
     return puzzle_input
 
 
-def read_file(filename, identifier):
+def read_file(filename, identifier, split_lines=True):
     full_path = ""
     if identifier == EXAMPLE_FILE_IDENTIFIER:
         full_path = path.joinpath(exampleFilesPath, filename)
@@ -25,4 +25,4 @@ def read_file(filename, identifier):
         full_path = path.joinpath(puzzleFilesPath, filename)
     with open(full_path, "r") as lines:
         puzzle_input = lines.read()
-    return puzzle_input.splitlines()
+    return puzzle_input.splitlines() if split_lines else puzzle_input
