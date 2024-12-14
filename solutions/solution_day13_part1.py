c7bfa7865bddd0eeb73e3d835e01e24e81a88079
Aditya Chaudhari <asc4896@gmail.com>
2024-12-14 12:19:48 +0530

    Day 13 Part 1 & 2 Solutions

---
+++ solutions/solution_day13_part1.py
@@ -0,0 +1,34 @@
+import constants
+from constants import *
+from utility.parseFile import *
+
+
+# Problem Statement: https://adventofcode.com/2024/day/13
+def solution(identifier):
+    puzzle_input = read_file("day13.txt", identifier)
+    # Write Solution here
+
+    machines = ("\n".join(puzzle_input)).split("\n\n")
+    coins = 0
+
+    for machine in machines:
+        btn_a, btn_b, prize = machine.split("\n")
+
+        btn_a = [*map(lambda i: int(i[2:]), btn_a.split(": ")[1].split(", "))]
+        btn_b = [*map(lambda i: int(i[2:]), btn_b.split(": ")[1].split(", "))]
+        prize = [*map(lambda i: int(i[2:]), prize.split(": ")[1].split(", "))]
+
+        times_b = (prize[1] * btn_a[0] - prize[0] * btn_a[1]) / (btn_b[1] * btn_a[0] - btn_b[0] * btn_a[1])
+        times_a = (prize[0] - btn_b[0] * times_b) / btn_a[0]
+
+        if 100 >= times_a >= 0 and 100 >= times_b >= 0 and times_a.is_integer() and times_b.is_integer():
+            coins += int(times_a) * 3 + int(times_b)
+
+    return coins
+
+def execute_example_file():
+    return solution(EXAMPLE_FILE_IDENTIFIER)
+
+
+def execute_puzzle_file():
+    return solution(PUZZLE_FILE_IDENTIFIER)
